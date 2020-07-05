5da3a4cb4705fb722a917766f6fc5f9bff9e1fc3
Yash Pradhan <54872329+mrpradhan555@users.noreply.github.com>
2020-07-05 16:00:58 +0530

    Update kelvinweather.js

--- javascript/kelvinweather.js
+++ javascript/kelvinweather.js
@@ -1,1 +1,7 @@
+const kelvin = 0;
+const celsius = kelvin - 273;
+let fahrenheit = celsius*(9/5) +32;
+console.log(`The temperature is ${Math.floor(fahrenheit)} degrees in Fahrenheit`);
 
+let newton = celsius * (33/100);
+console.log(`The temperature is  ${Math.floor(newton)} degrees on Newton`)
